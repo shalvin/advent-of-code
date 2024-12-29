31239b8a03dc5563c88b91506f08f01fa0e08897
Shalvin Deo <shalvin.deo@live.com>
2024-12-29 16:43:12 +0000

    Day 3 p2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -21,8 +21,52 @@ pub fn part_one(input: &str) -> Option<i64> {
     Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+#[derive(Clone, Copy, Debug)]
+enum Cmds {
+    Do,
+    Dont,
+    Mul(i64, i64),
+}
+
+pub fn part_two(input: &str) -> Option<i64> {
+    let pattern = Regex::new(r"(?<cmd>[\w\']+)\((?<params>\d+,\d+|)\)").unwrap();
+
+    let cmds: Vec<Cmds> = pattern
+        .captures_iter(input)
+        .filter_map(|caps| {
+            let (_, [cmd, params]) = caps.extract();
+            if cmd.ends_with("do") {
+                Some(Cmds::Do)
+            } else if cmd.ends_with("don't") {
+                Some(Cmds::Dont)
+            } else if cmd.ends_with("mul") {
+                let (lhs, rhs) = params.split_once(",").unwrap();
+                Some(Cmds::Mul(
+                    lhs.parse::<i64>().unwrap(),
+                    rhs.parse::<i64>().unwrap(),
+                ))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let mut enabled = true;
+    let mut sum = 0;
+    for cmd in cmds.iter() {
+        use Cmds::*;
+        match cmd {
+            Do => enabled = true,
+            Dont => enabled = false,
+            Mul(lhs, rhs) => {
+                if enabled {
+                    sum += lhs * rhs;
+                }
+            }
+        }
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -37,7 +81,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(54));
     }
 }
