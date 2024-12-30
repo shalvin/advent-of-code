5d53f0ef61416c46c6f0c5a72a7a4e19bfe9e2e7
Shalvin Deo <shalvin.deo@live.com>
2024-12-30 21:05:36 +0000

    Day 4 p2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -76,7 +76,50 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let input: Vec<String> = input.lines().map(|s| s.to_string()).collect();
+
+    let line_max = input.len();
+    let char_max = input[1].len();
+
+    let mut match_count = 0;
+    for line_idx in 1..(line_max - 1) {
+        for char_idx in 1..(char_max - 1) {
+            let c = input[line_idx].chars().nth(char_idx).unwrap();
+
+            if c != 'A' {
+                continue;
+            }
+
+            let (line_lower, char_lower, line_upper, char_upper) = (
+                (line_idx as i64 - 1),
+                (char_idx as i64 - 1),
+                (line_idx as i64 + 1),
+                (char_idx as i64 + 1),
+            );
+
+            let num_mas_found = [
+                ((line_lower, char_lower), (line_upper, char_upper)),
+                ((line_lower, char_upper), (line_upper, char_lower)),
+            ]
+            .into_iter()
+            .map(|((l1, c1), (l2, c2))| {
+                (
+                    input[l1 as usize].chars().nth(c1 as usize).unwrap(),
+                    input[l2 as usize].chars().nth(c2 as usize).unwrap(),
+                )
+            })
+            .filter(|(first, second)| {
+                (*first == 'M' && *second == 'S') || (*first == 'S' && *second == 'M')
+            })
+            .count();
+
+            if num_mas_found == 2 {
+                match_count += 1;
+            }
+        }
+    }
+
+    Some(match_count as u64)
 }
 
 #[cfg(test)]
@@ -92,6 +135,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
